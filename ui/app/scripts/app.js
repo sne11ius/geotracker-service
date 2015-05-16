13bedb90efe1c6a956a87084c0d0308ee28e2256
Cornelius Lilge <cornelius@wasis.nu>
2015-05-16 23:35:14 +0200

    New geotracker-service-app element

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -4,8 +4,8 @@
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  app.appName = 'Geotracker Service';
+  //var app = document.querySelector('#app');
+  //app.appName = 'Geotracker Service';
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
